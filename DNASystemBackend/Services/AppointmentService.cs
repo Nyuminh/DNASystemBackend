dbe0913542211e7badf6287c06642a0d697c1bed
khanhlinh00 <LINHNKSE140459@FPT.EDU.VN>
2025-06-21 18:12:30 +0700

    update CreateBookinh

--- DNASystemBackend/Services/AppointmentService.cs
+++ DNASystemBackend/Services/AppointmentService.cs
@@ -1,4 +1,5 @@
-﻿using DNASystemBackend.Interfaces;
+﻿using DNASystemBackend.DTOs;
+using DNASystemBackend.Interfaces;
 using DNASystemBackend.Models;
 
 namespace DNASystemBackend.Services
@@ -10,16 +11,28 @@ namespace DNASystemBackend.Services
         public AppointmentService(IAppointmentRepository repository)
         {
             _repository = repository;
-        }
-
-        public Task<IEnumerable<Booking>> GetAllAsync()
+        }        public Task<IEnumerable<Booking>> GetAllAsync()
             => _repository.GetAllAsync();
 
         public Task<Booking?> GetByIdAsync(string id)
-            => _repository.GetByIdAsync(id);
+            => _repository.GetByIdAsync(id);        public async Task<Booking> CreateAsync(Booking booking)
+        {
+            try
+            {
+                // Generate a new booking ID if not provided
+                if (string.IsNullOrEmpty(booking.BookingId))
+                {
+                    booking.BookingId = await _repository.GenerateBookingIdAsync();
+                }
 
-        public Task<Booking> CreateAsync(Booking booking)
-            => _repository.CreateAsync(booking);
+                await _repository.CreateAsync(booking);
+                return booking;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception($"Lỗi khi tạo đặt lịch: {ex.Message}", ex);
+            }
+        }
 
         public Task<bool> UpdateAsync(string id, Booking updated)
             => _repository.UpdateAsync(id, updated);
