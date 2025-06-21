481c76bff016f7f2b4aa025fc96fbbf7b30a0e15
Nyuminh <minhdlhse180733@fpt.edu.vn>
2025-06-22 01:25:22 +0700

    done register

--- DNASystemBackend/obj/Debug/net8.0/DNASystemBackend.AssemblyInfo.cs
+++ DNASystemBackend/obj/Debug/net8.0/DNASystemBackend.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DNASystemBackend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1102344cf2f47384b3d22741ad69811d798d1cb6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4c67e9b34784df56fa357f7d79190615147f4006")]
 [assembly: System.Reflection.AssemblyProductAttribute("DNASystemBackend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DNASystemBackend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
