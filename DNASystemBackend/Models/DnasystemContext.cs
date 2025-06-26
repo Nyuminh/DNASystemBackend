10f7f710bd5e580a0467f4795a30fbe0caaa31cc
Nyuminh <minhdlhse180733@fpt.edu.vn>
2025-06-26 21:29:00 +0700

    invoice

--- DNASystemBackend/Models/DnasystemContext.cs
+++ DNASystemBackend/Models/DnasystemContext.cs
@@ -35,9 +35,7 @@ public partial class DnasystemContext : DbContext
 
     public virtual DbSet<User> Users { get; set; }
 
-    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer(GetConnectionString());
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) => optionsBuilder.UseSqlServer(GetConnectionString());
     private string GetConnectionString() {
         return new ConfigurationBuilder()
             .SetBasePath(AppContext.BaseDirectory)
